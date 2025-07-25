da8b5730a980d5b164a96e505ad7227a9cce8f78
shalinth-adith <shalinth.adith@outlook.com>
2025-07-25 16:01:12 +0530

    Initial Commit

---
+++ LitLog/LitLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitLogApp.swift
+//  LitLog
+//
+//  Created by shalinth adithyan on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LitLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LitLogTests/LitLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  LitLogTests.swift
+//  LitLogTests
+//
+//  Created by shalinth adithyan on 25/07/25.
+//
+
+import Testing
+@testable import LitLog
+
+struct LitLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
