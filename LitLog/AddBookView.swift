e3cdd2016a35eed9357408d45c25b13012edf379
shalinth-adith <shalinth.adith@outlook.com>
2025-08-05 19:27:04 +0530

    Adding book

---
+++ LitLog/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  LitLog
+//
+//  Created by shalinth adithyan on 05/08/25.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
