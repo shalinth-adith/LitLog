1de029e471619e8b06f98272ad56ce8a9b846c1c
shalinth-adith <shalinth.adith@outlook.com>
2025-08-05 19:27:13 +0530

    Adding book

--- LitLog/AddBookView.swift
+++ LitLog/AddBookView.swift
@@ -4,12 +4,49 @@
 //
 //  Created by shalinth adithyan on 05/08/25.
 //
-
+import SwiftData
 import SwiftUI
 
 struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Science Fiction", "Mystery", "Historical", "Romance" , "Horror ", "Romantic" , "Thriller","Personal Development"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form{
+                Section{
+                    TextField("Name of the Book", text:$title)
+                    TextField("Author Name ", text:$author)
+                    Picker("Genre" , selection: $genre){
+                        ForEach(genres,id:\.self){
+                            Text($0)
+                        }
+                    }
+                }
+                Section("Write a Detailed Review"){
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection:$rating){
+                        ForEach(0..<11){
+                            Text(String($0))
+                        }
+                    }
+                }
+                Section{
+                    Button("Save"){
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
     }
 }
 
--- LitLog/ContentView.swift
+++ LitLog/ContentView.swift
@@ -4,16 +4,33 @@
 //
 //  Created by shalinth adithyan on 25/07/25.
 //
-
+import SwiftData
 import SwiftUI
 
 
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+     
+    @State private var showingAddBookSheet = false
+    
     var body: some View {
-                       
+        NavigationStack {
+            Text("count : \(books.count)")
+                .navigationTitle("LIT LOG")
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button("Add Book" , systemImage: "plus"){
+                            showingAddBookSheet.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddBookSheet){
+                    AddBookView()
+                }
             
-        
+        }
     }
 }
 
