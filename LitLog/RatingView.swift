7dd2d74912d0c14ced936a1dd24929bfa14dacf5
shalinth-adith <shalinth.adith@outlook.com>
2025-08-06 12:06:41 +0530

    added ratingview

---
+++ LitLog/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  LitLog
+//
+//  Created by shalinth adithyan on 06/08/25.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
